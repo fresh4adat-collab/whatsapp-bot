e6a93c8798e8f46b469c7e3c9b883e0784760a5f
fresh4adat-collab <fresh4adat@gmail.com>
2026-05-16 17:08:27 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
+
 const client = new Client({
     authStrategy: new LocalAuth(),
-
     puppeteer: {
         headless: true,
         executablePath: '/usr/bin/google-chrome',
